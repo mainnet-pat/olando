51133f1b6e4ec2e4ae9da4f9167e61ed0fd4443a
mainnet-pat <mainnet_pat@protonmail.com>
2025-06-27 08:43:01 +0300

    Release 0.0.7

--- dist/src/swap.d.ts
+++
@@ -1,7 +0,0 @@
-import { type TestNetWallet, type Wallet } from 'mainnet-js';
-import { type ActivePoolsResult } from './cauldron.js';
-export declare const getCauldronPools: (tokenId: string) => Promise<ActivePoolsResult>;
-export declare const buildSwapTransaction: (bchInvestmentSatoshis: bigint, wallet: Wallet | TestNetWallet, olandoCategory: string) => Promise<{
-    tradeTxList: import("cashlab/build/cauldron/types.js").TradeTxResult[];
-    pools: ActivePoolsResult;
-}>;
