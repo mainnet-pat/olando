bfd9c5ad7c082cafad75d6c12ed2d24f34f3cbc5
mainnet-pat <mainnet_pat@protonmail.com>
2025-09-20 13:12:36 +0300

    Release 0.0.30

---
+++ dist/artifacts/AuthGuard.artifact.js
@@ -0,0 +1,28 @@
+export default {
+    contractName: "AuthGuard",
+    constructorInputs: [
+        {
+            name: "authKeyTokenId",
+            type: "bytes"
+        }
+    ],
+    abi: [
+        {
+            name: "unlockWithNft",
+            inputs: [
+                {
+                    name: "keepGuarded",
+                    type: "bool"
+                }
+            ]
+        }
+    ],
+    bytecode: "OP_1 OP_UTXOTOKENCATEGORY OP_EQUALVERIFY OP_1 OP_UTXOTOKENAMOUNT OP_0 OP_NUMEQUALVERIFY OP_IF OP_0 OP_OUTPUTBYTECODE OP_INPUTINDEX OP_UTXOBYTECODE OP_EQUALVERIFY OP_ENDIF OP_1",
+    source: "",
+    compiler: {
+        name: "cashc",
+        version: "0.11.0"
+    },
+    updatedAt: "2025-09-20T10:12:33.808Z"
+};
+//# sourceMappingURL=AuthGuard.artifact.js.map
